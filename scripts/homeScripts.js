8db6d246a5a0f2574d4a212c36740f24bbca5095
Stefan Nychka <stefannychka@gmail.com>
2022-10-19 21:30:41 -0600

    dynamically populate states drop-down, remove other html

--- scripts/homeScripts.js
+++ scripts/homeScripts.js
@@ -1,1 +1,10 @@
-console.log('hello');
+
+function populateDropdown(items) {
+    const div = document.querySelector('.dropdown-menu');
+    items.forEach(item => {
+        // <li><a class="dropdown-item" href="#">state 1 </a></li>
+        div.innerHTML += `<li><a class="dropdown-item" href="#">${item}</a></li>`;
+    });
+}
+
+populateDropdown(locationsArray);
