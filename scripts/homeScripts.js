e16494fd49f9eba0e0916300f5a58f566298531f
Stefan Nychka <stefannychka@gmail.com>
2022-10-19 23:30:50 -0600

    get 2nd drop-down working

--- scripts/homeScripts.js
+++ scripts/homeScripts.js
@@ -8,26 +8,44 @@ function populateDropdown(selector, items) {
     });
 }
 
-let state;
-
+let state, parkType;
+ 
 // displays the parks in selector.
 // uses global var. state 
 function displayParks(selector) {
     let stateResults = nationalParksArray.filter(park => park.State === state);
+    let typeResults = nationalParksArray.filter(park => park.LocationName.includes(parkType));
+
+    let results = [];
+    if (stateResults.length > 0 && typeResults.length > 0) {
+        results = Array.from(new Set(stateResults.filter(item => typeResults.includes(item)))); 
+        state = ""; parkType = "";
+    } else {
+        results = stateResults.length > 0 ? stateResults : typeResults;
+    }
+
     const ul = document.querySelector(selector);
-    ul.innerHTML = ''; // beyond the scope
-    stateResults.forEach(park => {
+    ul.innerHTML = ''; 
+    results.forEach(park => {
         ul.innerHTML += `<li>${park.LocationName}</li>`;
     });
+
 }
 
 
 // https://stackoverflow.com/questions/70561282/how-to-link-event-logic-to-bootstrap-5-dropdown-clicks
 // https://getbootstrap.com/docs/5.2/components/dropdowns/#events
-// gets the selected state, stores it in global var state, triggers displaying the states
+// gets the selected state, stores it in global var state, triggers displaying the parks 
 const statesDropdown = document.getElementById('statesDropdown');
 statesDropdown.addEventListener('hide.bs.dropdown', event => {
     state = event.clickEvent.target.innerHTML;
+    displayParks('#parks');
+});
+
+// gets the selected park type, stores it in global var parkType, triggers displaying parks
+const parkTypesDropdown = document.getElementById('parkTypesDropdown');
+parkTypesDropdown.addEventListener('hide.bs.dropdown', event => {
+    parkType = event.clickEvent.target.innerHTML;
     displayParks('#parks');
 });
 
