1cdd4fdbf11be4a595f1f31deded71205f537d70
Stefan Nychka <stefannychka@gmail.com>
2022-10-20 00:05:26 -0600

    improve UI (@ the expense of wonky logic)

--- scripts/homeScripts.js
+++ scripts/homeScripts.js
@@ -9,6 +9,16 @@ function populateDropdown(selector, items) {
 }
 
 let state, parkType;
+
+function setState(val) {
+    state = val;
+    document.getElementById("state").textContent = val === "" ? "(non-chosen)" : val;
+}
+
+function setParkType(val) {
+    parkType = val;
+    document.getElementById("parktype").textContent = val === "" ? "(non-chosen)" : val;
+}
  
 // displays the parks in selector.
 // uses global var. state 
@@ -18,8 +28,8 @@ function displayParks(selector) {
 
     let results = [];
     if (stateResults.length > 0 && typeResults.length > 0) {
-        results = Array.from(new Set(stateResults.filter(item => typeResults.includes(item)))); 
-        state = ""; parkType = "";
+        results = Array.from(new Set(stateResults.filter(item => typeResults.includes(item))));
+        //setState(""); setParkType("");
     } else {
         results = stateResults.length > 0 ? stateResults : typeResults;
     }
@@ -38,14 +48,18 @@ function displayParks(selector) {
 // gets the selected state, stores it in global var state, triggers displaying the parks 
 const statesDropdown = document.getElementById('statesDropdown');
 statesDropdown.addEventListener('hide.bs.dropdown', event => {
-    state = event.clickEvent.target.innerHTML;
+    // side-effect: if both dropdowns have values, null park type
+    if (parkType !== "" && state !== "") {setParkType("");}
+    setState(event.clickEvent.target.innerHTML);
     displayParks('#parks');
 });
 
 // gets the selected park type, stores it in global var parkType, triggers displaying parks
 const parkTypesDropdown = document.getElementById('parkTypesDropdown');
 parkTypesDropdown.addEventListener('hide.bs.dropdown', event => {
-    parkType = event.clickEvent.target.innerHTML;
+    // side-effect: if both dropdowns have values, null state
+    if (parkType !== "" && state !== "") {setState("");}
+    setParkType(event.clickEvent.target.innerHTML);
     displayParks('#parks');
 });
 
