e0a4496334a6844423bea0f4467208ee4e232527
Stefan Nychka <stefannychka@gmail.com>
2022-10-23 22:27:45 -0600

    update logic for standard selects

--- scripts/parksScript.js
+++ scripts/parksScript.js
@@ -1,8 +1,7 @@
 
-// populates the bootstrap drop-down at selector
+// populates the select element
 // with the passed-in array, items
-function populateDropdown(id, items) {
-    const element = document.getElementById(id);
+function populateDropdown(element, items) {
     items.forEach(item => {
         element.appendChild(new Option(item, item))
     });
@@ -65,6 +64,51 @@ parkTypesDropdown.addEventListener('hide.bs.dropdown', event => {
 });
 */
 
+let statesList = document.getElementById('statesList');
+let parkTypesList = document.getElementById('parkTypesList');
+let parks = document.getElementById('parks');
 
-populateDropdown('statesList', locationsArray);
-populateDropdown('parkTypesList', parkTypesArray);
+// https://stackoverflow.com/questions/42853028/removing-li-from-ul-in-for-loop-js
+function clearList(ul) {
+    const length = ul.children.length;
+    for (let i = 0; i < length; i++) {
+        ul.removeChild(ul.children[0]);
+    }
+}
+
+// displays the parks in parks.
+function displayParks() {
+    clearList(parks);
+    let state = statesList.value;
+    let stateResults = [];
+    if (state.length > 0) {
+        stateResults = nationalParksArray.filter(park => park.State === state);
+    }
+
+    let parkType = parkTypesList.value;
+    let typeResults = [];
+    if (parkType.length > 0) {
+        typeResults = nationalParksArray.filter(park => park.LocationName.includes(parkType));
+    }
+
+    let results = [];
+    if (stateResults.length > 0 && typeResults.length > 0) {
+        results = Array.from(new Set(stateResults.filter(item => typeResults.includes(item))));
+    } else {
+        results = stateResults.length > 0 ? stateResults : typeResults;
+    }
+
+    results.forEach(park => {
+        const li = document.createElement("li");
+        const text = document.createTextNode(`${park.LocationName}`);
+        li.appendChild(text);
+        parks.appendChild(li);
+    });
+
+}
+
+
+populateDropdown(statesList, locationsArray);
+populateDropdown(parkTypesList, parkTypesArray);
+
+
