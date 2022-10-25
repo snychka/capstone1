1e4aabcd660ee3c468125508eaf80464871e4381
snychka <stefannychka@gmail.com>
2022-10-24 21:37:15 -0600

    just note a likely different approach to clearing

--- scripts/library.js
+++ scripts/library.js
@@ -9,6 +9,8 @@ function populateDropdown(element, items) {
 
 // https://stackoverflow.com/questions/42853028/removing-li-from-ul-in-for-loop-js
 // https://developer.mozilla.org/en-US/docs/Web/API/Node/removeChild#simple_examples
+// for a ul, the following should work!
+//   function clearList(ul) { let a = Array.from(ul.children); a.forEach(x => ul.removeChild(x)); }
 function clearList(ul) {
     while (ul.firstChild) {
         ul.removeChild(ul.firstChild);
