832354167638e5756f83e805581df5c4cb3a34ba
Stefan Nychka <stefannychka@gmail.com>
2023-05-22 11:29:32 -0600

    clear table first

--- scripts/parks.js
+++ scripts/parks.js
@@ -1,31 +1,49 @@
-let location_select = document.getElementById("location");
-let parks_table = document.getElementById("parks_table");
+"use string";
 
-function find_park(event) {
+let location_select = document.getElementById("location");
+let parks_tbody = document.getElementById("parks_table"); 
+
+
+function display_parks_by_location(event) {
+    
+    function find_parks(state) {
+        return nationalParksArray.filter(park => park.State === state);
+    }
+    
+    function insert_parks_into_table(tbody, parks) {
+        parks.forEach(park => {
+            let row = tbody.insertRow();
+            let state_cell = row.insertCell();
+            let name_cell = row.insertCell();
+            state_cell.appendChild(document.createTextNode(park.State));
+            name_cell.appendChild(document.createTextNode(park.LocationName));
+            /*
+            let row = document.createElement('tr');
+            let state_cell = document.createElement('td');
+            let name_cell = document.createElement('td');
+            let state = document.createTextNode(park.State);
+            let name = document.createTextNode(park.LocationName);
+            state_cell.appendChild(state);
+            name_cell.appendChild(name);
+            row.appendChild(state_cell);
+            row.appendChild(name_cell);
+            parks_table.appendChild(row);
+            */
+        });
+    }
+
+    // uncertain if safe/recommended
+    function clear_table(tbody) {
+        tbody.innerHTML = '';
+    }
+
     let target_state = event.srcElement.value;
-    let parks = nationalParksArray.filter(park => park.State === target_state);
-    parks.forEach(park => {
-        let row = parks_table.insertRow();
-        let state_cell = row.insertCell();
-        let name_cell = row.insertCell();
-        state_cell.appendChild(document.createTextNode(park.State));
-        name_cell.appendChild(document.createTextNode(park.LocationName));
-        /*
-        let row = document.createElement('tr');
-        let state_cell = document.createElement('td');
-        let name_cell = document.createElement('td');
-        let state = document.createTextNode(park.State);
-        let name = document.createTextNode(park.LocationName);
-        state_cell.appendChild(state);
-        name_cell.appendChild(name);
-        row.appendChild(state_cell);
-        row.appendChild(name_cell);
-        parks_table.appendChild(row);
-        */
-    });
+    let parks = find_parks(target_state);
+    clear_table(parks_tbody);
+    insert_parks_into_table(parks_tbody, parks);
 }
 
-location_select.addEventListener("change", find_park);
+location_select.addEventListener("change", display_parks_by_location);
 
 function populate_select(select, data) {
     data.forEach(element => {
