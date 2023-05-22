0a3d67eb53040919d2a96e39ab1c482b981d5899
Stefan Nychka <stefannychka@gmail.com>
2023-05-22 07:37:06 -0600

    populate location drop-down

--- scripts/parks.js
+++ scripts/parks.js
@@ -1,1 +1,13 @@
-console.log('test');
+
+let location_select = document.getElementById("location");
+
+function populate_select(select, data) {
+    data.forEach(element => {
+        var option = document.createElement("option");
+        option.textContent = element;
+        option.value = element;
+        select.appendChild(option);
+    });
+}
+
+populate_select(location_select, locationsArray);
